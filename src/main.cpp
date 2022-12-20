490a1e5095d0581f9edf50868fd79d87dbc31df1
Emil <emil.reyier.hedin@gmail.com>
2022-12-20 01:17:03 +0100

    Reformat

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
+
 #define NUM_SPEAKERS 8
 
-uint32_t musicTable[][7] = {};
+uint32_t musicTable[][3] = {};
 int musicLen = 0;
 
-//uint32_t playingNotes[7] = {};
-//int freeSpeakers = 7;
-//int clocksPerClick = 24;
 int ticksPerBeat = 480;
 int usPerBeat = 500000;
-uint32_t usPerTick = usPerBeat/ticksPerBeat;
+uint32_t usPerTick = usPerBeat / ticksPerBeat;
 
 void setup() {
     Serial.begin(115200);
@@ -31,33 +29,36 @@ void setup() {
     ledcAttachPin(32, 10);
     ledcAttachPin(4, 12);
     ledcAttachPin(16, 14);
+
+    Serial.println("Boot successful");
 }
 
 void loop() {
     for (int i = 0; i < musicLen; i++) {
         uint32_t newFreq = musicTable[i][1];
         uint32_t channel = musicTable[i][0];
+
         if (channel == 9) {
             usPerBeat = newFreq;
-            usPerTick = usPerBeat/ticksPerBeat;
+            usPerTick = usPerBeat / ticksPerBeat;
         } else {
-            ledcWriteTone(channel*2, newFreq);
+            ledcWriteTone(channel * 2, newFreq);
         }
 
         //Serial.printf("Waiting %u us\n", musicTable[i+1][2] * usPerTick);
-        delayMicroseconds(musicTable[i+1][2] * usPerTick);
+        delayMicroseconds(musicTable[i + 1][2] * usPerTick); // TODO: Change to system based on micros().
     }
 
     delay(2000);
 
     for (int channel = 0; channel < NUM_SPEAKERS; channel++) {
-        ledcWriteTone(channel*2, 240);
+        ledcWriteTone(channel * 2, 240);
     }
 
     delay(1000);
 
     for (int channel = 0; channel < NUM_SPEAKERS; channel++) {
-        ledcWriteTone(channel*2, 0);
+        ledcWriteTone(channel * 2, 0);
     }
 
     delay(3000);
